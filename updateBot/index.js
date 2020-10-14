e96529c0aa23f4886885b432c5ea9bcab4a7071e
Patricio Toledo <patotomastoledo6@gmail.com>
2020-10-14 00:22:44 +0000

    saque el harcode

--- updateBot/index.js
+++ updateBot/index.js
@@ -126,7 +126,7 @@ const start = async(bots) => {
                 cookies = bot.cookies;
                 if (bot.action === "follow") {
                     userName = bot.follow.pop();
-                    status = await follow("aracelihache", cookies, bot.ratio);
+                    status = await follow(userName, cookies, bot.ratio);
                     //Static = sigue siempre a los mismos. Dynamic, los sigue, los deja de seguir y ahi queda	
                     console.log("El status es",status)
                     //Solo lo dejo de seguir si es el status que bussco	
